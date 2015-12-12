604b4ee15431a5bd2200cd6db67dca76edc2763c
unknown <josebragv@gmail.com>
2015-12-12 19:09:08 +0000

    Spawn multiple waves and difficulty scaling.

--- unityProject/Assets/Scripts/obstaclesSpawner.cs
+++ unityProject/Assets/Scripts/obstaclesSpawner.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class obstaclesSpawner : MonoBehaviour {
     public float randomPositionMultiplier;
     public float avoidCollisionRadius = 5f;
+    public float currentEnemiesSpeed = 5f;
+    public float speedIncrementAfterWave = 1.05f;
 
 	// Use this for initialization
 	void Start () {      	        
@@ -14,23 +16,25 @@ public class obstaclesSpawner : MonoBehaviour {
 	
 	}
 
-    public void spawnWave(GameObject[] waveToSpawn)
-    {
+    public void spawnWave(GameObject[] waveToSpawn){
         foreach(GameObject obstacle in waveToSpawn)
         {
             this.spawnNextObstacle(obstacle);
         }
+        increaseCurrentEnemiesSpeed();
     }
 
-    private void spawnNextObstacle(GameObject obstacle) {
+    private void spawnNextObstacle(GameObject obstacle){
         Vector3 randomSpawnPosition = (Random.insideUnitSphere * randomPositionMultiplier) + this.transform.position;
-        if (Physics.OverlapSphere(randomSpawnPosition, avoidCollisionRadius).Length > 0)
-        {
+        if (Physics.OverlapSphere(randomSpawnPosition, avoidCollisionRadius).Length > 0){
             spawnNextObstacle(obstacle);
+        }else{
+            GameObject spawnedObtacle = Instantiate(obstacle, randomSpawnPosition, Quaternion.identity) as GameObject;
+            spawnedObtacle.GetComponent<obstacleActions>().movementSpeed = currentEnemiesSpeed;
         }
-        else
-        {
-            Instantiate(obstacle, randomSpawnPosition, Quaternion.identity);
-        }
+    }
+
+    private void increaseCurrentEnemiesSpeed(){
+        currentEnemiesSpeed = currentEnemiesSpeed * speedIncrementAfterWave;
     }
 }
