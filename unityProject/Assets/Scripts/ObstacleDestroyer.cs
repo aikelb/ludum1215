c6e29d80a06b0b8120569495589c4ca670e78b0c
Aitor Lozano <info@aitorlozano.com>
2015-12-12 18:34:31 +0000

    push obstacles destroyer

---
+++ unityProject/Assets/Scripts/ObstacleDestroyer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleDestroyer : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag("Obstacle"))
+			Destroy(other.gameObject);
+	}
+	
+}
--- unityProject/Assets/Scripts/obstacleActions.cs
+++ unityProject/Assets/Scripts/obstacleActions.cs
@@ -5,19 +5,18 @@ public class obstacleActions : MonoBehaviour {
     public GameObject player;
     public float randomPositionMultiplier;
     public float movementSpeed = 25f;
-    private Vector3 endPosition;
 
 	// Use this for initialization
-	void Start () { 
+	/*void Start () { 
         endPosition = (Random.insideUnitSphere * randomPositionMultiplier) + player.transform.position;
         endPosition.z = player.transform.position.z;
         endPosition.x = this.transform.position.x;
         endPosition.y = this.transform.position.y;
-    }
+    }*/
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position += (endPosition - transform.position).normalized * movementSpeed * Time.deltaTime;
+        transform.position += Vector3.back * movementSpeed * Time.deltaTime;
     }
 
     private void didHitPlayer() {
