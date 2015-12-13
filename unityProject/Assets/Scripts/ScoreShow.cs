01e472592f73e79fc556b9c3ce60128d764f3dc2
unknown <josebragv@gmail.com>
2015-12-13 18:29:07 +0000

    Score on Gameover screen.

--- unityProject/Assets/Scripts/ScoreShow.cs
+++ unityProject/Assets/Scripts/ScoreShow.cs
@@ -1,16 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ScoreShow : MonoBehaviour {
+    public GameObject scoreTextObject;
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Hola");
-        Debug.Log(PlayerPrefs.GetFloat("Score"));
+        changeScoreText(PlayerPrefs.GetFloat("Score"));
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void changeScoreText(float score) {
+        scoreTextObject.GetComponent<Text>().text = score.ToString();
+    }
 }
