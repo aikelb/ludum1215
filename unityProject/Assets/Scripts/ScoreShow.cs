a4839edaeb427223716cee87f01cca3484901a7a
unknown <josebragv@gmail.com>
2015-12-13 18:18:26 +0000

    Sounds balanced.

---
+++ unityProject/Assets/Scripts/ScoreShow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreShow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hola");
+        Debug.Log(PlayerPrefs.GetFloat("Score"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
