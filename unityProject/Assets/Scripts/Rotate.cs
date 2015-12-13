570ef1022f88b15d6ad91a0523549a8a9601cfb4
Aitor Lozano <info@aitorlozano.com>
2015-12-13 19:42:15 +0000

    merge fix

--- unityProject/Assets/Scripts/Rotate.cs
+++ unityProject/Assets/Scripts/Rotate.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class Rotate : MonoBehaviour {
 
+	public bool randomize = false;
 	public float speed = 1;
+	
+	Vector3 direction;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (randomize) {
+			direction = Random.insideUnitSphere;
+			speed = Random.value > 0.5 ? 1 : -1;
+		} else
+			direction = Vector3.forward;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+		transform.Rotate(direction, speed * Time.deltaTime);
 	}
 }
