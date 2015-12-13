ad78034905cb057bb8df80333ffcd1111ce9346b
Aitor Lozano <info@aitorlozano.com>
2015-12-13 11:55:25 +0000

    moar things

--- unityProject/Assets/Scripts/SoundManager.cs
+++ unityProject/Assets/Scripts/SoundManager.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 
 public class SoundManager : MonoBehaviour {
 
-	public AudioClip hit;
-	public AudioClip explosion;
-	public AudioClip shoot;
+	public AudioClip obstacleHit;
+	public AudioClip obstacleDestroyed;
+	public AudioClip weaponShoot;
+	public AudioClip shipHit;
+	public AudioClip shipDestroyed;
+	public AudioClip shipHealed;
 	
 	Dictionary<AudioClip, int> priority;
 
@@ -18,33 +21,53 @@ public class SoundManager : MonoBehaviour {
 		aSource = GetComponent<AudioSource>();
 		stamps = new float[] {0,0,0};
 		priority = new Dictionary<AudioClip, int>();
-		priority.Add(hit, 0);
-		priority.Add(explosion, 1);
-		priority.Add(shoot, 2);
+		priority.Add(obstacleHit, 0);
+		priority.Add(obstacleDestroyed, 1);
+		priority.Add(weaponShoot, 2);
+		priority.Add(shipHit, 3);
+		priority.Add(shipDestroyed, 4);
 	}
 	
 	void OnEnable () {
 		Gun.OnShoot += HandleOnShoot;
 		obstacleActions.OnGotShot += HandleOnObstacleShoot;
 		obstacleActions.OnDestroyed += HandleOnObstacleDestroyed;
+		Ship.OnHit += HandleOnShipHit;
+		Ship.OnHealUp += HandleOnShipHeal;
+		Ship.OnDestroyed += HandleOnShipDestroyed;
 	}
 	
 	void OnDisable () {
 		Gun.OnShoot -= HandleOnShoot;
 		obstacleActions.OnGotShot -= HandleOnObstacleShoot;
 		obstacleActions.OnDestroyed -= HandleOnObstacleDestroyed;
+		Ship.OnHit -= HandleOnShipHit;
+		Ship.OnHealUp -= HandleOnShipHeal;
+		Ship.OnDestroyed -= HandleOnShipDestroyed;
 	}
 	
 	void HandleOnShoot () {
-		Play(shoot);
+		Play(weaponShoot);
 	}
 	
 	void HandleOnObstacleShoot (Vector3 p) {
-		Play(hit);
+		Play(obstacleHit);
 	}
 	
 	void HandleOnObstacleDestroyed (Vector3 p, int score) {
-		
+		Play(obstacleDestroyed);
+	}
+	
+	void HandleOnShipHit (Vector3 p, int hp) {
+		Play(shipHit);
+	}
+	
+	void HandleOnShipHeal (int hp) {
+		Play(shipHealed);
+	}
+	
+	void HandleOnShipDestroyed (Vector3 position) {
+		Play(shipDestroyed);
 	}
 	
 	void Play (AudioClip a) {
