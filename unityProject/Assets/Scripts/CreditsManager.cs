17f1c4a3ea8d5b34b5fbe14397ea806082739909
unknown <josebragv@gmail.com>
2015-12-14 12:23:46 +0000

    Credits on menu fixed no transition.

---
+++ unityProject/Assets/Scripts/CreditsManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsManager : MonoBehaviour {
+    public CanvasGroup mainCanvas;
+    public CanvasGroup creditsCanvas;
+
+    public void showCredits() {
+        mainCanvas.alpha = 0;
+        creditsCanvas.alpha = 1;
+    }
+
+    public void hideCredits() {
+        mainCanvas.alpha = 1;
+        creditsCanvas.alpha = 0;
+    }
+}
